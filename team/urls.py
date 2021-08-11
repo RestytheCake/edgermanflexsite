4e5ec65897aa15aad42bdf775444d27aa20e03b5
Arthur Ditte <RestysDevAcc@outlook.de>
2021-08-11 11:02:39 +0200

    Update again URL

--- team/urls.py
+++ team/urls.py
@@ -4,34 +4,34 @@ from django.urls import path
 app_name = 'team'
 urlpatterns = [
     path('', views.nick, name='home'),
-    path('/videos/', views.videos, name='video'),
+    path('videos/', views.videos, name='video'),
     # Forum
-    path('/forum/', views.forum_view, name='forum'),
-    path('/forum/comment/', views.comment_view, name='comment'),
-    path('/forum/create', views.forum_create, name='create'),
-    path('/forum/search/', views.search_post, name='message_search'),
-    path('/forum/filter', views.advfilter, name='advfilter'),
-    path('/forum/searchworker/', views.searchworker_view, name='searchworker'),
+    path('forum/', views.forum_view, name='forum'),
+    path('forum/comment/', views.comment_view, name='comment'),
+    path('forum/create', views.forum_create, name='create'),
+    path('forum/search/', views.search_post, name='message_search'),
+    path('forum/filter', views.advfilter, name='advfilter'),
+    path('forum/searchworker/', views.searchworker_view, name='searchworker'),
     # Forum End
-    path('/posts/', views.posts, name='posts'),
-    path('/contact/', views.contacts, name='contact'),
-    # Profile
-    path('/profile/', views.profile_view, name='profile'),
-    path('/profile/fa/', views.fa, name='fa'),
-    path('/profileadd', views.profile_add, name='profile_add'),
-    path('/notification', views.notification_view, name='notification'),
-    path('/settings', views.settings_view, name='settings'),
+    path('posts/', views.posts, name='posts'),
+    path('contact/', views.contacts, name='contact'),
+    # Prole
+    path('profile/', views.profile_view, name='profile'),
+    path('profile/fa/', views.fa, name='fa'),
+    path('profileadd', views.profile_add, name='profile_add'),
+    path('notification', views.notification_view, name='notification'),
+    path('settings', views.settings_view, name='settings'),
     # Account
-    path('/login/', views.login_view, name='login_view'),
-    path('/register/', views.register, name='register'),
-    path('/logout/', views.logout_view, name='logout'),
+    path('login/', views.login_view, name='login_view'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.logout_view, name='logout'),
     # Old Site
-    path('/sale/', views.sale, name='sale'),
+    path('sale/', views.sale, name='sale'),
     # For Supporter
-    path('/supporter/', views.supporter, name='supporter'),
+    path('supporter/', views.supporter, name='supporter'),
     # Help
-    path('/problems', views.problems, name='problem'),
+    path('problems', views.problems, name='problem'),
     # Rickroll
-    path('/tests', views.rickroll, name='rickroll'),
+    path('tests', views.rickroll, name='rickroll'),
 
 ]
